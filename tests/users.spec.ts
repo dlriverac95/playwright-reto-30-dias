684ebfb8f8ace5825d04f6e5f2b411bf83a3b32d
David Rivera <dlriverac.87@gmail.com>
2026-06-22 22:05:26 -0500

    reto 6 completado

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -51,4 +51,33 @@ test.describe('HRM users table', () => {
         }
         console.log(employeeNames)
     });
+
+
+    test('Select specific user for edition', async ({ page }) => {
+        await page.getByRole('link', { name: 'Admin' }).click()
+        await page.getByRole('navigation', { name: 'Topbar Menu' }).getByText('User Management').click()
+        await page.getByRole('menuitem', { name: 'Users' }).click()
+
+        await expect(page.getByRole('table').getByRole('row').nth(1)).toBeVisible()
+        const validRows = page.getByRole('table').getByRole('row')
+        const usernames: string[] = [];
+        for (let i = 1; i < await validRows.count(); i++) {
+            var username = await validRows.nth(i).getByRole('cell').nth(1).innerText();
+            if (username !== 'Admin') {
+                usernames.push(username);
+            }
+        }
+
+        const randomIndex = Math.floor(Math.random() * usernames.length);
+        const selectedUsername = usernames[randomIndex];
+
+        const pencilButton = page.getByRole('table').getByRole('row').filter({ hasText: selectedUsername }).locator('button').filter({ has: page.locator('i.bi-pencil-fill') });
+        await pencilButton.click();
+
+
+        const usernameInput = page.locator("//label[contains(.,'Username')]/parent::div/following-sibling::div/input")
+        await expect(usernameInput).toHaveValue(selectedUsername);
+        console.log(`Selected user for editing: ${selectedUsername}`);
+        console.log(`Username input value: ${await usernameInput.inputValue()}`);
+    });
 });
